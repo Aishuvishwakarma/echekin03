e6bad6a1de3924603d3354b8e4b3abf712a0fa28
aishu <vishwakarmaa949@gmail.com>
2021-07-07 09:04:58 +0530

    'final07'

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,62 +1,39 @@
 import React from 'react'
 import './ContactUs.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function ContactUs() {
     return (
         <>
-        <div className="contact">
-         <div className="container">
-        <div className="row d-flex flex-column p-5 align-items-center text-center">
-        <h2 id='heading'>Contact us</h2>
-        <p>We're here to help and always happy<br />hear from you!</p>
-         </div>
-         <div className="row d-flex flex-column p-5 align-items-center text-center">
-        <p>Contact us using the form below. if you need help<br />instantly,ckeck out our <Link to='/help'>Help center</Link></p>or use the chat
-         </div>
-         <div className="row form">
-         <div id="formsctn">
-         <form action="">
-             <h2>First Name <span>*</span></h2>
-             <input type="text" />
-             <h2>First Name <span>*</span></h2>
-             <input type="text" />
-             <h2>First Name <span>*</span></h2>
-             <input type="text" />
-             <h2>First Name <span>*</span></h2>
-             <input type="text" />
-             <h2>First Name <span>*</span></h2>
-             <input type="text" />
-             <h2>First Name <span>*</span></h2>
-             <input id='msg' type="text" />
-             <input type="submit" value='submit'/>
-         </form>
-         </div>
-        </div>
+            <div className="contact  d-flex align-items-center justify-content-center">
+                <div className="container bg-light">
+                    <div className="row d-flex flex-column p-2 align-items-center justify-content-center text-center">
+                        <h2>Contact us</h2>
+                        <p className='fw-normal mt-2'>We're here to help and always happy<br />hear from you!</p>
+                        <p className='mt-4 fw-bolder text-secondary'>Contact us using the form below. if you need help<br />instantly,ckeck out our <Link to='/help'>Help center</Link>or use the chat</p>
+                    </div>
+                </div>
             </div>
-         </div>
-        <div className="contact d-flex justify-content-center align-items-center text-center">
-            <div className="container">
-            <div className="row d-flex flex-column align-items-center text-center">
-            <h2 id="heading">Other Contacts</h2>
-            </div>
-            <div className="row d-flex flex-column align-items-center text-center">
-            <h2 id="heading">Asia/India</h2>
-            <div id='cntactdtl' className="col">
-                <p>Phone number : <span>+91 9584762135</span></p>
-                <p>Email Id : <span>eCheckin@gmail.com</span></p>
-                <p>Address : <span>1 North Bridge Road,8th floor found 8,179094 singapore</span></p>
-            </div>
-            <h2 id="heading">America</h2>
-            <div id='cntactdtl' className="col">
-                <p>Phone number : <span>+31 9584762135</span></p>
-                <p>Email Id : <span>eCheckin@gmail.com</span></p>
-                <p>Address : <span>1 North Bridge Road,8th floor found 8,179094 Califorina</span></p>
-            </div>
-            </div>
-            </div>
-        </div>
+            <div className="formSection  d-flex align-items-center justify-content-center">
+                <div className="container d-flex align-items-center justify-content-center">
+                    <div className="formContainer p-5">
+                        <div className="crcl"><i className="ri-phone-fill"></i></div>
+                            <form action="">
+                                <h5 className="form-label fw-normal text-secondary mt-2">Full Name</h5>
+                                <input type="text" className="form-control" />
+                                <h5 className="form-label fw-normal text-secondary mt-2">Email address</h5>
+                                <input type="email" className="form-control" />
+                                <h5 className="form-label fw-normal text-secondary mt-2">Contact</h5>
+                                <input type="email" className="form-control" />
+                                <h5 className="form-label fw-normal text-secondary mt-2">Write something</h5>
+                                <textarea type="email" className="form-control" />
+                                <button className='btn btn-primary mt-3'>Submit</button>
+                            </form>
+                            
+                        </div>
+                    </div>
+                </div>
         </>
-    )
+            )
 }
 
-export default ContactUs
+            export default ContactUs
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,38 +1,32 @@
 import React from 'react'
 import './Footer.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function Footer() {
     return (
-        <div className='footer d-flex justify-content-center align-items-center'>
-            <div className="row-8 container">
-            <div className="col-4">
-                <h2>eCheckin</h2>
-                <p>Contact visitor system</p>
-                <div className="social">
-                <i class="fab fa-facebook-square"></i>
-                <i class="fab fa-twitter-square"></i>
-                <i class="fab fa-linkedin"></i>
-                <i class="fab fa-instagram"></i>
+        <div className="footer">
+            <div className="container h-100 d-flex flex-column align-items-center justify-content-center">
+                <div className="linkSctn">
+                    <div className="links p-3">
+                        <h4>eCheckin</h4>
+                        <p>Contact visitor system</p>
+                    </div>
+                    <div className="links p-3">
+                        <Link className='footerLinks'>Security</Link>
+                        <Link className='footerLinks'>Compliance</Link>
+                        <Link className='footerLinks'>Efficiency</Link>
+                        <Link className='footerLinks'>Branding</Link>
+                        <Link className='footerLinks'>Enterprise services</Link>
+                        <Link className='footerLinks'>Integration</Link>
+                        <Link className='footerLinks'>Covid-19 features</Link>
+                        <Link className='footerLinks'>All features</Link>
+                    </div>
+                    <div className="links p-3">
+                        <h4>Contact Us</h4>
+                        <Link className='footerLinks'><i class="fas fa-envelope"></i>salestecboot.mumbai@gmail.com</Link>
+                        <Link className='footerLinks'><i class="fas fa-globe"></i>website</Link>
+                    </div>
                 </div>
-            </div>
-            <div className="col-4">
-            <h4>Product</h4>
-            <Link  className='footerLinks'>Security</Link>
-            <Link className='footerLinks'>Compliance</Link>
-            <Link className='footerLinks'>Efficiency</Link>
-            <Link className='footerLinks'>Branding</Link>
-            <Link className='footerLinks'>Enterprise services</Link>
-            <Link className='footerLinks'>Integration</Link>
-            <Link className='footerLinks'>Covid-19 features</Link>
-            <Link className='footerLinks'>All features</Link>
-            <Link className='footerLinks' style={{marginTop:'50px'}} id='echekinRight' >2020 eCheckin. All Right Reseverd.</Link>
-            </div>
-            <div className="row-4" id='footermailSection'>
-                <h4>Contact Us</h4>
-               <Link className='footerLinks'><i class="fas fa-envelope"></i>salestecboot.mumbai@gmail.com</Link>
-               <Link className='footerLinks'><i class="fas fa-globe"></i>website</Link>
-               <Link className='footerLinks' style={{marginTop:'50px'}} id='echekinRight' >2020 eCheckin. All Right Reseverd.</Link>
-            </div>
+                <Link id='echekinRight' >2020 eCheckin. All Right Reseverd.</Link>
             </div>
         </div>
     )
